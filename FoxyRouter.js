5fe9a9bb427dd6547fb24b3cf956b27ae5be1f1b
Will Brock <willbrock@Wills-MacBook-Air.local>
2016-09-12 20:47:36 -0400

    Initial commit

---
+++ FoxyRouter.js
@@ -0,0 +1,132 @@
+((context) => {
+	`use strict`;
+
+	class FoxyRouter {
+		constructor() {
+			// Stores all the routes for the application
+			this.routes = [];
+		}
+
+		// Add new routes
+		add(route) {
+			// Get any callbacks
+			let callbacks = Array.from(arguments);
+			callbacks.shift();
+
+			this.routes.push({
+				path      : route,
+				callbacks : callbacks || null
+			});
+
+			return this;
+		}
+
+		// Main function to rendor the route data
+		render(active_route, search) {
+			// Store any route parameters, if any
+			let parameters = {};
+
+			// Regex that will match values like :id, :user_id
+			let route_regex   = /:[^\s(/|\-|\.)]+/g;
+			let replace_regex = `[\\w-]+`;
+
+			// Find a matching route
+			for(let route of this.routes) {
+				let path      = route.path;
+				let callbacks = route.callbacks;
+
+				// Replace any parameters with regex to match the path
+				let path_test = path.replace(route_regex, replace_regex);
+				let match     = active_route.match(path_test);
+
+				// If there is no match for the current route
+				if(!match) {
+					continue;
+				}
+
+				// Get any keys and values that are in the route, e.g :id, :user_id, etc.
+				path.replace(route_regex, (match, offset) => {
+					parameters[match.substr(1)] = active_route.substr(offset - 1).match(replace_regex)[0];
+				});
+
+				// Check for any query parameters
+				search.replace(/([^?=&]+)(=([^&]*))?/g, (match, key, foo, value) => {
+					parameters[key] = decodeURIComponent(value);
+				});
+
+				// Trigger the callbacks
+				for(let callback of callbacks) {
+					callback(parameters);
+				}
+
+				return true;
+			}
+
+			// If we get here then no route was found
+			return false;
+		}
+	}
+
+	if(typeof define === `function` && define.amd) {
+		define(new FoxyRouter());
+	}
+	else if(typeof module != `undefined` && module.exports) {
+		module.exports = new FoxyRouter();
+	}
+	else {
+		context.FoxyRouter = new FoxyRouter();
+	}
+
+	// Handle history
+	window.addEventListener(`popstate`, (e) => {
+		render(window.location.pathname, window.location.search);
+	});
+
+	// Render the route on page load
+	// This will also handle bookmarked links
+	window.addEventListener(`load`, (e) => {
+		let route  = window.location.pathname;
+		let search = window.location.search;
+
+		render(route, search);
+		e.preventDefault();
+	});
+
+	// Route all clicks on link tags except for external links
+	document.querySelector(`body`).addEventListener(`click`, (e) => {
+		let target = e.target;
+
+		// Only route on link tags
+		if(target.tagName.toLowerCase() !== `a`) {
+			return;
+		}
+
+		// Don't continue if it's an external link
+		if(target.host !== window.location.host) {
+			return;
+		}
+
+		// Render the route
+		let rendered = render(target.pathname, target.search);
+
+		// Don't refresh on internal links that have a route defined
+		if(rendered) {
+			e.preventDefault();
+		}
+	});
+
+	/**
+	 * Helper function to render routes
+	 * @param  {String} Path of the route
+	 * @param  {String} Query parameters
+	 * @return {Boolean}
+	 */
+	function render(route, search = ``) {
+		// Set the new path and search
+		window.history.pushState({}, ``, `${route}${search}`);
+
+		// Render the new route
+		return Routes.render(route, search);
+	}
+
+})(this);
---
+++ README.md
@@ -0,0 +1,114 @@
+# Foxy Router
+
+Lightweight javascript routing library for single page applications
+
+## Features
+	* Express style route matching
+	* Bookmarking of links
+	* Uses the history api instead of hashes
+
+## Requirements
+
+ES6. Use babel if you would like support prior to ES6.
+
+## Quick Start
+
+Registering a new route:
+
+```javascript
+// This route will mach a url like: /stocks/AAPL
+FoxyRouter.add(`/stocks/:stock_symbol`, (parameters) => {
+	// This matches a link to: /stocks/AAPL
+	document.getElementById(`page-container`).innerHTML = `You're viewing data for stock symbol: ${parameters.stock_symbol}`;
+})
+```
+
+## Basic Route Paths
+
+Paths can be any string along with regular expression patterns.
+
+Route to the website index
+
+```javascript
+FoxyRouter.add(`/`, () => {
+	document.getElementById(`page-container`).innerHTML = `This is the index`;
+});
+```
+
+Route to the about page
+
+```javascript
+FoxyRouter.add(`/about`, () => {
+	document.getElementById(`page-container`).innerHTML = `This is the about page`;
+});
+```
+
+## Regular Expression Route Paths
+
+
+## Route Parameters
+
+Routes can also have dynamic parameters. These follow the same rules as express routes.
+
+```javascript
+FoxyRouter.add(`/triathlon/:race_name`, (parameters) => {
+	// :race_name is the triathlon specified
+	// e.g. /triathlon/Dunedin
+
+	document.getElementById(`page-container`).innerHTML = `You are viewing the ${parameters.race_name} triathlon`;
+});
+```
+
+```javascript
+FoxyRouter.add(`triathlon/:race_name/:athlete, () => {
+	// :race_name is the triathlon specified
+	// e.g. /triathlon/Dunedin
+	// :athlete is that persons data for the triathlon
+	// e.g. /triathlon/Dunendin/Will_Brock
+
+	document.getElementById(`page-container`).innerHTML = `You are viewing the ${parameters.race_name} triathlon for ${parameters.athlete}`;
+});
+```
+
+## Query Parameters
+
+All query parameters are merged into the route parameters argument
+
+```javascript
+	// e.g /stocks/AAPL?timeframe=weekly
+	FoxyRouter.add('/stocks/:stock_symbol', (parameters) => {
+		// parameters.stock_symbol = AAPL
+		// parameters.timeframe = weekly
+	});
+```
+
+
+## Route chaining
+
+Adding new routes can be chained
+
+```javascript
+FoxyRouter
+	.add(`/`, (parameters) => {
+
+	})
+	.add(`/about`, (parameters) => {
+
+	})
+	.add(`/about_us`, (parameters) => {
+
+	});
+```
+
+## Callbacks
+
+Multiple callbacks can be added for each route
+
+```javascript
+FoxyRouter
+	.add('/stocks', (parameters) => {
+		console.log(`Callback 1 for /stocks`);
+	}, () => {
+		console.log(`Callback 2 for /stocks`);
+	});
+```
